a1b2497cedc09e432aae18c2a749fdf8d5055388
jay-juns <necrozerg2@naver.com>
2023-02-02 14:39:50 +0900

    네임 추가

---
+++ src/component/search/Search.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import type { NextPage } from 'next';
+import { useLazyQuery, gql } from '@apollo/client';
+
+const COUNTRIES_SEARCH_QUERY = gql`
+    query GetCountries($code: String) {
+        countries(filter: {code: { eq: $code }}) {
+            name
+            code
+            emojiU
+        }
+    }
+`;
+
+const Search: NextPage  = () => {
+    const [searchFilter, setSearchFilter] = useState('');
+    const [executeSearch, { data }] = useLazyQuery(
+        COUNTRIES_SEARCH_QUERY
+    );
+
+    return (
+        <>
+            <div className='search-bar'>
+                <span>검색하기</span>
+                <input
+                    type="text"
+                    onChange={(e) => setSearchFilter(e.target.value)}
+                />
+                <button
+                    onClick={() =>
+                        executeSearch({
+                            variables: { 
+                                code: searchFilter.toUpperCase()
+                            }
+                        })
+                    }
+                >
+                    확인
+                </button>
+            </div>
+            <div>
+                {data && data.countries.map((countrie: any, index: number) => (
+                    <div key={index} style={{display:'flex',alignItems:'center',color: 'blue'}}>
+                        <p style={{color: 'red',marginRight: '15px'}}>{countrie.name}</p>
+                        <span style={{fontSize: '12px'}}>{countrie.code}</span>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Search
